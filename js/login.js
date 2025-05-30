a7b21caf0f86e4dedc4f30f9bcaada9654180113
CarolinaExosk <01000101ac@gmail.com>
2025-05-29 22:31:33 -0300

    First commit

---
+++ js/login.js
@@ -0,0 +1,26 @@
+
+const input = document.querySelector('.login__input');
+    const button = document.querySelector('.login__btn');
+    const form = document.querySelector('.login-form');
+
+    const validateInput = ({ target }) => {
+        if (target.value.length > 4) {
+            button.removeAttribute('disabled');
+            return;
+        }
+        button.setAttribute('disabled', '');
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        localStorage.setItem('Exosk', input.value);
+        window.location = '../pages/jogo.html';
+    };
+
+    input.addEventListener('input', validateInput);
+    form.addEventListener('submit', handleSubmit);
+
+    button.classList.add('loading');
+    setTimeout(() => {
+        window.location = 'pages/jogo.html';
+    }, 1000);
